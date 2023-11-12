680dfc91e36a1cdaa9eb0f1219d23e0e960b87cf
Jacoai <banan0012354@gmail.com>
2023-11-12 19:26:39 +0300

    refactor ListBuilders

--- lib/pages/home_page/home_page_view.dart
+++ lib/pages/home_page/home_page_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:ui_clen_api_vt/pages/home_page/home_page_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -69,49 +70,72 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ),
               ),
-              BlocBuilder<HomePageBloc, HomePageState>(
-                builder: (context, state) {
-                  return Column(
-                    children: [
-                      //Queque
-                      ListView.builder(
-                        scrollDirection: Axis.vertical,
-                        shrinkWrap: true,
-                        itemCount: state.pathsToScan.length,
-                        padding: const EdgeInsets.all(10),
-                        itemBuilder: (BuildContext context, int index) {
-                          return Container(
-                            child: Text(state.pathsToScan[index],
-                                style: const TextStyle(fontSize: 22)),
-                          );
-                        },
-                      ),
-                      Column(
-                        children: [
-                          if (state.virusTotalData.isNotEmpty)
-                            ...state.virusTotalData.map((data) => Text(
-                                'Source: ${data.source} \nHarmless:${data.harmless} '
-                                '\nMalicious:${data.malicious} \nSuspicious:${data.suspicious}'
-                                '\nUndetected:${data.undetected}\nTimeout:${data.timeout}\n'
-                                'Date ${DateTime.fromMillisecondsSinceEpoch(data.time * 1000)}\n')),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          const Text('Посмотреть историю'),
-                          ElevatedButton(
-                            onPressed: () {
-                              Navigator.pushNamed(context, '/history');
-                              //   Navigator.pushReplacementNamed(
-                              //       context, '/history');
+              Expanded(
+                child: BlocBuilder<HomePageBloc, HomePageState>(
+                  builder: (context, state) {
+                    return Column(
+                      children: [
+                        //Queque
+                        Expanded(
+                          child: ListView.builder(
+                            scrollDirection: Axis.vertical,
+                            shrinkWrap: true,
+                            itemCount: state.pathsToScan.length,
+                            padding: const EdgeInsets.all(10),
+                            itemBuilder: (BuildContext context, int index) {
+                              return Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  border: Border.all(
+                                    color:
+                                        const Color.fromARGB(255, 255, 165, 0),
+                                    width: 4,
+                                  ),
+                                ),
+                                padding: const EdgeInsets.all(3.0),
+                                child: Text(
+                                  state.pathsToScan[index],
+                                  style: const TextStyle(fontSize: 14),
+                                ),
+                              );
                             },
-                            child: const Text('Посмотреть историю'),
                           ),
-                        ],
-                      )
-                    ],
-                  );
-                },
+                        ),
+                        Expanded(
+                          flex: 3,
+                          child: ListView.builder(
+                            scrollDirection: Axis.vertical,
+                            shrinkWrap: true,
+                            itemCount: state.virusTotalData.length,
+                            padding: const EdgeInsets.all(10),
+                            itemBuilder: (BuildContext context, int index) {
+                              return Container(
+                                child: Text(
+                                    'Source: ${state.virusTotalData[index].source} \nHarmless:${state.virusTotalData[index].harmless} '
+                                    '\nMalicious:${state.virusTotalData[index].malicious} \nSuspicious:${state.virusTotalData[index].suspicious}'
+                                    '\nUndetected:${state.virusTotalData[index].undetected}\nTimeout:${state.virusTotalData[index].timeout}\n'
+                                    'Date ${DateTime.fromMillisecondsSinceEpoch(state.virusTotalData[index].time * 1000)}\n'
+                                    ''),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/history');
+                    //   Navigator.pushReplacementNamed(
+                    //       context, '/history');
+                  },
+                  child: const Text('Посмотреть историю'),
+                ),
               ),
             ],
           ),
