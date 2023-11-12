19bb026b9633cc59831073f395356f50e5ffd031
Jacoai <banan0012354@gmail.com>
2023-11-12 19:26:05 +0300

    create database repository

--- lib/utilities/injectable.config.dart
+++ lib/utilities/injectable.config.dart
@@ -11,8 +11,9 @@
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import 'repositories/check_virus_repository.dart' as _i3;
-import 'usecases/check_virus_usecase.dart' as _i4;
+import 'repositories/check_virus_repository.dart' as _i4;
+import 'repositories/database_repository.dart' as _i3;
+import 'usecases/check_virus_usecase.dart' as _i5;
 
 extension GetItInjectableX on _i1.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -25,9 +26,12 @@ extension GetItInjectableX on _i1.GetIt {
       environment,
       environmentFilter,
     );
-    gh.singleton<_i3.ReportCheckRepository>(_i3.ReportCheckRepository());
-    gh.singleton<_i4.VirusTotalCheckUsecase>(
-        _i4.VirusTotalCheckUsecase(gh<_i3.ReportCheckRepository>()));
+    gh.singleton<_i3.DatabaseRepository>(_i3.DatabaseRepository());
+    gh.singleton<_i4.ReportCheckRepository>(_i4.ReportCheckRepository());
+    gh.singleton<_i5.VirusTotalCheckUsecase>(_i5.VirusTotalCheckUsecase(
+      gh<_i4.ReportCheckRepository>(),
+      gh<_i3.DatabaseRepository>(),
+    ));
     return this;
   }
 }
