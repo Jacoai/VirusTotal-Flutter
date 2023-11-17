34377ca796b6d36e9a81fb25662bfa9464670044
Jacoai <banan0012354@gmail.com>
2023-11-17 15:18:20 +0300

    refactor: replate isFile function

--- lib/utilities/usecases/check_virus_usecase.dart
+++ lib/utilities/usecases/check_virus_usecase.dart
@@ -2,6 +2,7 @@ import 'package:injectable/injectable.dart';
 import 'package:ui_clen_api_vt/utilities/repositories/check_virus_repository.dart';
 import 'package:ui_clen_api_vt/utilities/repositories/check_history_repository.dart';
 import 'package:virus_total_cli/virus_total_cli.dart';
+import 'package:ui_clen_api_vt/utilities/functions/is_file_.dart';
 
 @singleton
 class VirusTotalCheckUsecase {
@@ -15,7 +16,7 @@ class VirusTotalCheckUsecase {
 
   Future<VirusTotalData> check(String path) async {
     try {
-      bool isFile = _isFile(path);
+      bool isFile1 = isFile(path);
       VirusTotalData data;
 
       if (await databaseRepository.containsKey(path)) {
@@ -23,8 +24,8 @@ class VirusTotalCheckUsecase {
         data = tmpData!;
       } else {
         AnalysisData analysisData =
-            await _reportCheckRepository.getAnalisisReport(path, isFile);
-        data = convertAnalysisDataToVirusTotalData(analysisData, path, isFile);
+            await _reportCheckRepository.getAnalisisReport(path, isFile1);
+        data = convertAnalysisDataToVirusTotalData(analysisData, path, isFile1);
         await databaseRepository.put(path, analysisData);
       }
       return data;
@@ -35,15 +36,5 @@ class VirusTotalCheckUsecase {
 
   void show() {
     databaseRepository.printConsole();
-  }
-
-  bool _isFile(String path) {
-    RegExp urlRegex = RegExp(
-        r"((https?:www\.)|(https?:\/\/)|(www\.))[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9]{1,6}(\/[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)?");
-
-    if (path.contains(urlRegex)) {
-      return false;
-    }
-    return true;
   }
 }
