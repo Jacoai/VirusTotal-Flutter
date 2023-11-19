377588c59b9df0f274b7c39c6ad1915d26a2d528
Jacoai <banan0012354@gmail.com>
2023-11-19 12:21:08 +0300

    add new color

--- lib/components/color_palettes/color_palette.dart
+++ lib/components/color_palettes/color_palette.dart
@@ -7,3 +7,4 @@ const Color trueBlue = Color.fromRGBO(64, 98, 187, opacity);
 const Color mintGreen = Color.fromRGBO(233, 255, 249, opacity);
 const Color malachite = Color.fromRGBO(85, 232, 103, opacity);
 const Color bringPink = Color.fromRGBO(244, 91, 105, opacity);
+const Color amethyst = Color.fromRGBO(138, 90, 192, opacity);
